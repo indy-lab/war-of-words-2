4bb1dc2ddc7b34df5625bf625d2b675485a93c91
Victor Kristof <victor.kristof@epfl.ch>
2021-02-10 10:19:25 +0100

    Fix names of baseline models

--- lib/warofwords/models/baselines.py
+++ lib/warofwords/models/baselines.py
@@ -1,13 +1,13 @@
-import numpy as np
-
 from collections import Counter, defaultdict
 from dataclasses import dataclass
-from ..features import ParameterVector, Features
+
+import numpy as np
+
+from ..features import Features, ParameterVector
 from .base import Model, TrainedModel
 
 
-class Random(Model):
-
+class Naive(Model):
     @dataclass
     class Hyperparameters:
         pass
@@ -22,8 +22,7 @@ class Random(Model):
         return {'params': ParameterVector([])}, -1
 
 
-class TrainedRandom(TrainedModel):
-
+class TrainedNaive(TrainedModel):
     def __init__(self, features, hyperparams, params):
         super().__init__()
 
@@ -47,65 +46,7 @@ class TrainedRandom(TrainedModel):
         return loss / len(data)
 
 
-class StrongRandom(Model):
-
-    @dataclass
-    class Hyperparameters:
-        pass
-
-    def __init__(self, data, features, hyperparameters, verbose):
-        self.data = data
-        super().__init__()
-
-    def log_likelihood(self, params):
-        return 0
-
-    def fit(self):
-        # Compute proportion of time the dossier wins.
-        count = 0
-        for featmat, label in self.data:
-            if label == featmat.shape[0] - 1:
-                count += 1
-        p = count / len(self.data)
-        # Store this probability p that the dossier wins.
-        features = Features()
-        features.add('p')
-        params = features.new_parameters()
-        params['p'] = p
-        return {'params': params}, -1
-
-
-class TrainedStrongRandom(TrainedModel):
-
-    def __init__(self, features, hyperparams, params):
-        self.parameters = params
-        super().__init__()
-
-    def probabilities(self, X):
-        # Number of conlficts between MEPs.
-        n_conflicts = len(X) - 1
-        p = self.parameters['p']
-        # Probability vector tuned to the probability that the dossier wins.
-        return [(1-p)/n_conflicts] * n_conflicts + [p]
-
-    def accuracy(self, data):
-        acc = 0
-        for X, y in data:
-            prob = self.probabilities(X)
-            if np.argmax(prob) == y:
-                acc += 1
-        return acc / len(data)
-
-    def log_loss(self, data):
-        loss = 0
-        for X, y in data:
-            prob = self.probabilities(X)
-            loss -= np.log(prob[y])
-        return loss / len(data)
-
-
-class StrongestRandom(Model):
-
+class Random(Model):
     @dataclass
     class Hyperparameters:
         pass
@@ -128,8 +69,9 @@ class StrongestRandom(Model):
             countconflicts.append(size)
         countconflicts = Counter(countconflicts)
         # Compute prior probability of dossier winning.
-        p = (sum([v for v in countwins.values()])
-             / sum([v for v in countconflicts.values()]))
+        p = sum([v for v in countwins.values()]) / sum(
+            [v for v in countconflicts.values()]
+        )
         # Compute prior per dossier.
         ps = dict()
         features = Features()
@@ -145,8 +87,7 @@ class StrongestRandom(Model):
         return {'params': params}, -1
 
 
-class TrainedStrongestRandom(TrainedModel):
-
+class TrainedRandom(TrainedModel):
     def __init__(self, features, hyperparams, params):
         self.parameters = params
         super().__init__()
@@ -158,7 +99,7 @@ class TrainedStrongestRandom(TrainedModel):
         key = 'p-' + str(size)
         p = self.parameters[key]
         # Probability vector tuned to the probability that the dossier wins.
-        return [(1-p)/n_conflicts] * n_conflicts + [p]
+        return [(1 - p) / n_conflicts] * n_conflicts + [p]
 
     def accuracy(self, data):
         acc = 0
