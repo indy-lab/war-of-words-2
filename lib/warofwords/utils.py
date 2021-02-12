1c578ad372d6fc1082a73de05eeac851cd42b327
Victor Kristof <victor.kristof@epfl.ch>
2021-02-12 11:45:48 +0100

    Adapt utils to work with new filename convention

--- lib/warofwords/utils.py
+++ lib/warofwords/utils.py
@@ -1,8 +1,8 @@
+import pickle
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import numpy as np
-import pickle
-
-from pathlib import Path
 
 
 def get_base_dir(file):
@@ -13,27 +13,27 @@ def get_base_dir(file):
 def parse_definition(definition):
     leg = definition['legislature']
     explicit = definition.get('explicit-features', None)
-    text = definition.get('text-features', None)
+    text = definition.get('text-features', False)
     latent = definition.get('latent-features', False)
-    by_dossier = definition.get('by-dossier', False)
-    random = definition.get('random', None)
-    return leg, explicit, text, latent, by_dossier, random
+    chronological = definition.get('chronological', False)
+    baseline = definition.get('baseline', None)
+    return leg, explicit, text, latent, chronological, baseline
 
 
-def build_name(leg, explicit, text, latent, by_dossier, random):
+def build_name(leg, explicit, text, latent, chronological, baseline):
     name = f'ep{leg}'
-    if random is not None:
-        name += f'-{random}'
+    if baseline is not None:
+        name += f'-{baseline}'
     else:
         if explicit is None:
             raise ValueError('You must specify some explicit features')
         name += f'-{explicit}'
         if latent:
             name += '-latent'
-        if text is not None:
-            name += f'-{text}'
-    if by_dossier:
-        name += '-by_dossier'
+        if text:
+            name += '-text'
+    if chronological:
+        name += '-chronological'
     return name
 
 
@@ -44,8 +44,10 @@ def load_pkl(path):
 
 
 def display_parameters(group, features, params, n=10):
-    feats = [(features.get_name(idx), params[features.get_name(idx)])
-             for idx in features.get_group(group)]
+    feats = [
+        (features.get_name(idx), params[features.get_name(idx)])
+        for idx in features.get_group(group)
+    ]
     for name, param in sorted(feats, key=lambda k: k[1], reverse=True)[:n]:
         if param != 0:
             print(f'{param:.4f} {name}')
@@ -73,31 +75,40 @@ def barchart(y7, y8, obj, config, figpath=None):
     bars = [[y7[i], y8[i]] for i in range(len(obj))]
 
     r0 = np.arange(len(bars[0]))
-    rs = [[x*0.75 + i * width + i * offset for x in r0]
-          for i in range(len(bars))]
+    rs = [
+        [x * 0.75 + i * width + i * offset for x in r0]
+        for i in range(len(bars))
+    ]
 
     fig, ax = plt.subplots(figsize=(3.5, 1.9))
 
     lines = list()
     for i, (r, ys) in enumerate(zip(rs, bars)):
-        line = plt.bar(r, ys,
-                       width=width,
-                       color=config['colors'][i],
-                       linewidth=1,
-                       edgecolor=config['edgecolors'][i],
-                       hatch=config['patterns'][i],
-                       label=obj[i])
+        line = plt.bar(
+            r,
+            ys,
+            width=width,
+            color=config['colors'][i],
+            linewidth=1,
+            edgecolor=config['edgecolors'][i],
+            hatch=config['patterns'][i],
+            label=obj[i],
+        )
         lines.append(line)
 
     # Add xticks on the middle of the group bars
     # plt.xlabel('group', fontweight='bold')
     plt.ylabel(config['ylabel'])
     # plt.xticks([r*0.75 + 2*width + 2*offset for r in range(len(r0))],
-    plt.xticks([r*0.75 + width + offset for r in range(len(r0))],
-               [r'7\textsuperscript{th} legislature',
-                r'8\textsuperscript{th} legislature'])
+    plt.xticks(
+        [r * 0.75 + width + offset for r in range(len(r0))],
+        [
+            r'7\textsuperscript{th} legislature',
+            r'8\textsuperscript{th} legislature',
+        ],
+    )
 
-    plt.ylim([0., 1.])
+    plt.ylim([0.0, 1.0])
     plt.legend(
         lines,
         obj,
@@ -105,7 +116,7 @@ def barchart(y7, y8, obj, config, figpath=None):
         frameon=True,
         fontsize='x-small',
         framealpha=0.9,
-        markerscale=0.1
+        markerscale=0.1,
     )
     plt.tight_layout()
     if figpath is not None:
@@ -121,7 +132,7 @@ def get_value(arr):
 def k_fold_gen(data, k_fold=10):
     subset_size = int(len(data) / k_fold)
     for k in range(k_fold):
-        train = data[:k * subset_size] + data[(k + 1) * subset_size:]
-        valid = data[k * subset_size:][:subset_size]
+        train = data[: k * subset_size] + data[(k + 1) * subset_size :]
+        valid = data[k * subset_size :][:subset_size]
 
         yield train, valid
